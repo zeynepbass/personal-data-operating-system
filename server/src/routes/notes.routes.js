95bc41aa7bb15a2ae0d377f4b386c35aee62b9b5
zeynep bas <101248567+zeynepbass@users.noreply.github.com>
2026-08-15 15:49:29 +0300

    -

--- server/src/routes/notes.routes.js
+++ server/src/routes/notes.routes.js
@@ -3,11 +3,12 @@ import express from "express";
 import {
   getNotes,
   createNote,
+  deleteNote
 } from "../controllers/notes.controller.js";
 
 const router = express.Router();
 
 router.get("/", getNotes);
 router.post("/", createNote);
-
+router.delete("/:id", deleteNote);
 export default router;
